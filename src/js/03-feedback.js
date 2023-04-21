72d95ed19f52c1a60a83a9a10393130d2bd50ec8
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-04-21 10:56:09 +0300

    add 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+const throttle = require('lodash.throttle');
+const userDate = {}||''
+const form = document.querySelector('.feedback-form')
+checkLocalStorage()
+form.addEventListener('input', throttle(onInput,500))
+form.addEventListener('submit', onSubmit)
+function onInput(event) {
+    userDate[event.target.name] = event.target.value
+    localStorage.setItem("feedback-form-state", JSON.stringify(userDate))
+}
+function checkLocalStorage() {
+    const stateForm = JSON.parse(localStorage.getItem("feedback-form-state"))
+    if (stateForm) {
+        const { email, message } = stateForm
+        form.email.value = email
+        form.message.value = message
+    }
+}
+function onSubmit(event) {
+    event.preventDefault()
+    localStorage.clear()
+    event.target.reset()
+}
 
