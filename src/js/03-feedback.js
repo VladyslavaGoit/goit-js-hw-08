2acb9c097139d3e359606b5a81a7751449c65d56
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-04-22 10:51:50 +0300

    Add cleaning object

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,30 @@
 const throttle = require('lodash.throttle');
-const userDate = {}||''
+let userDate = {}
 const form = document.querySelector('.feedback-form')
-checkLocalStorage()
 form.addEventListener('input', throttle(onInput,500))
 form.addEventListener('submit', onSubmit)
 function onInput(event) {
     userDate[event.target.name] = event.target.value
+    // console.log(userDate)
     localStorage.setItem("feedback-form-state", JSON.stringify(userDate))
+    // console.log(localStorage.getItem("feedback-form-state"))
 }
 function checkLocalStorage() {
     const stateForm = JSON.parse(localStorage.getItem("feedback-form-state"))
+    // console.log(stateForm)
+    // console.log(userDate)
     if (stateForm) {
         const { email, message } = stateForm
-        form.email.value = email
-        form.message.value = message
+        form.email.value = email||''
+        form.message.value = message||''
     }
 }
+checkLocalStorage()
 function onSubmit(event) {
     event.preventDefault()
-    localStorage.clear()
+    console.log(userDate)
+    localStorage.removeItem("feedback-form-state")
     event.target.reset()
+    userDate = {}
 }
 
